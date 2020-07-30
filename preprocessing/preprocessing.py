709e3d6c7a1fc15b13e7f52a57bd0b1bd0f82b01
Jacob Atkins <jhatkins@StoryArks-MacBook-Air.local>
2020-07-30 14:16:50 -0500

    created preprocessing as mainfile, added data folders

---
+++ preprocessing/preprocessing.py
@@ -0,0 +1,296 @@
+import os
+import argparse
+import json
+import time
+import re
+import glob
+from collections import defaultdict
+from shutil import copyfile
+from nltk.corpus import stopwords
+
+from config import params
+from utils import *
+# from get_pdfs.src.data_utils import * --remove
+
+from pyzotero import zotero
+
+
+def load_data(load_json, zot_library, collection_id, verbose = params["DEFAULT_VERBOSE"]):
+    start_time = time.time()
+    if 'data_sets.json' in list(os.listdir("data/json")) and load_json:
+        with open("data/json/data_sets.json") as json_data:
+            datasets = json.load(json_data)
+    else:
+        with open("data/json/cmr_datasets.json") as json_cmr_datasets_file:
+            json_cmr_datasets = json.load(json_cmr_datasets_file)
+        json_cmr_dataset_list = json_cmr_datasets["feed"]["entry"]
+        datasets = create_data_sets(json_cmr_dataset_list)
+
+    if 'itemDict.json' in list(os.listdir("data/json")) and load_json:
+        if verbose >= 1:
+            print("Loading values from itemDict.json...")
+        with open("data/json/itemDict.json", "r") as json_file:
+            items = json.load(json_file)
+    else:
+        if verbose >=1:
+            print("Getting data from Zotero...")
+        items = zot_library.everything(zot_library.collection_items(collection_id))
+        with open('data/json/itemDict.json', "w") as outfile:
+            json.dump(items, outfile, indent=4)
+    if verbose >= 1:
+        print("Data loaded. It took %s seconds to load the data.\n"
+            "There were %s items loaded"%(time.time() - start_time, len(items)))
+
+    return items, datasets
+
+
+def sort_zotero_parts(zotero_items):
+    articles = {}
+    notes = {}
+    attachments = {}
+    other = {}
+    lookup_child_notes = defaultdict(list)
+    lookup_child_links = defaultdict(list)
+
+    for item in zotero_items:
+        if item['data']['itemType'] == 'note':
+            notes[item['key']] = item
+            lookup_child_notes[item['data']['parentItem']].append(item['key'])
+        elif item['data']['itemType'] == 'journalArticle':
+            articles[item['key']] = item
+        elif item['data']['itemType'] == 'attachment':
+            attachments[item['key']] = item
+            lookup_child_links[item['data']['parentItem']].append(item['key'])
+        else:
+            other[item['key']] = item
+    n = open("data/json/notes.json", "w")
+    j = open("data/json/articles.json", "w")
+    a = open("data/json/attachments.json", "w")
+    json.dump(notes, n, indent=4)
+    json.dump(articles, j, indent=4)
+    json.dump(attachments, a, indent=4)
+    n.close()
+    j.close()
+    a.close()
+
+    return articles, notes, attachments, lookup_child_notes, lookup_child_links
+
+
+# add verbose statements
+def process_zotero_citations(notes, lookup, data_set_citations_file, verbose=params["DEFAULT_VERBOSE"]):
+    citation_list = []
+    citation_count = defaultdict(int)
+    for article in lookup:
+        publication_id = article
+        for key in lookup[article]:
+            # NOTE: You may need to go back and fix the labels to ensure there is a new note for every dataset
+            tags = notes[key]['data']['tags']
+            mention = re.sub('(</p>)|(<p>)|(<br />)|(<br/>)', '', notes[key]['data']['note'])
+            if "</span>" in mention:
+                mention = mention.split(">")[1]
+                mention = " ".join(mention.split("</span"))
+            for tag in tags:
+                if 'dataset' not in tag['tag']:
+                    continue
+                citation = search(citation_list, publication_id, tag)
+                if citation:
+                    for m in mention.split("\n"):
+                        citation['mention_list'].append(m)
+                else:
+                    mention_list = []
+                    for m in mention.split("\n"):
+                        mention_list.append(m)
+                    citation_list.append({
+                        "citation_id": len(citation_list) + 1,
+                        "publication_id": publication_id,
+                        "data_set_id": tag['tag'].split(":")[1],
+                        "mention_list": mention_list,
+                        "score": 1.0
+                    })
+                    citation_count[tag['tag'].split(":")[1]] += 1
+    with open(data_set_citations_file, "w") as fp:
+        json.dump(citation_list, fp, indent=4)
+    if verbose >= 1:
+        print("There were %s total citations found." %len(citation_list))
+        if verbose > 1:
+            pass # Add additionalverbosity
+        elif verbose > 2:
+            pass # Add additional verbosity
+    return citation_list
+
+
+# add verbose statements
+def process_zotero_publications(zotero_path, publication_ids, articles, attachments, lookup, verbose=params["DEFAULT_VERBOSE"]):
+    publication_list = []
+    publication_count = defaultdict(int)
+    for publication_id in publication_ids:
+        if search_pubs(publication_list, publication_id):
+            continue
+        # try:
+        #     pub_item = articles[publication_id]
+        # except KeyError:
+        #     continue
+        # try:
+        #     author = pub_item['data']['creators'][0]["lastName"]
+        #     title = pub_item['data']['title']
+        # except KeyError:
+        #     pass
+        # nums = re.findall(r'\d{4}', pub_item['data']['date'])
+        # year = None
+        # pdf_name = None
+        # if nums:
+        #     year = nums[0]
+        #     pdf_name = year+'_'
+        # if author:
+        #     pdf_name = pdf_name+author+'_'
+        # pdf_name = pdf_name+title[0:50]
+        # if ':' in pdf_name:
+        #     pdf_name = pdf_name.split(':')[0]
+        # #print ('PDF name: %s' % (pdf_name))
+        # pdf_name = re.sub('(<)|(>)', '', pdf_name)
+        # pdf_name = re.sub('/', '-', pdf_name)
+        keys = lookup[publication_id]
+        for key in keys:
+            pdf_name = attachments[key]['data']['filename']
+        pdf_files = glob.glob(zotero_path + "**/" + pdf_name)
+        if pdf_files:
+            pdf_file = pdf_files[0]  # os.path.basename(pdf_files[0])
+            txt_name = publication_id + '.txt'
+        else:
+            continue
+        pub_item = articles[publication_id]
+        nums = re.findall(r'\d{4}', pub_item['data']['date'])
+        year = None
+        if nums:
+            year = nums[0]
+        publication_list.append({
+            "publication_id": publication_id,
+            "unique_identifier": pub_item['data']['DOI'],
+            "title": pub_item['data']['title'],
+            "pub_date": year,
+            "pdf_file_name": pdf_file,
+            "text_file_name": txt_name
+        })
+        name = publication_id + ".pdf"
+        if name in os.listdir("data/PDF"):
+            continue
+        else:
+            copyfile(pdf_file, "data/PDF/" + name)
+        publication_count[publication_id] += 1
+    with open("data/json/publications.json", "w") as fp:
+        json.dump(publication_list, fp, indent=4)
+    if verbose >= 1:
+        print("There were %s total publications found." %len(publication_list))
+        if verbose > 1:
+            pass # Add additional verbosity
+        elif verbose > 2:
+            pass # Add additional verbosity
+
+    return publication_list
+
+
+def split_doc_paragraphs(src, dest, remove_stopwords = params["REMOVE_STOPWORDS"],
+                     labels = params['PARAGRAPH_LABELS'], verbose=params["DEFAULT_VERBOSE"]):
+    stop_words = stopwords.words("english")
+    max_len = len(max(labels, key=len))
+
+    for doc in os.listdir(src):
+        fp = open(src + doc)
+        txt = fp.read()
+        paragraphs = txt.split("\n\n")
+        outtxt = ""
+        for i in range(len(paragraphs)):
+            para = paragraphs[i]
+            if any(label.lower() in para.lower() for label in labels):
+                if len(para) > 3 * max_len:
+                    outtxt += para
+                else:
+                    outtxt += paragraphs[i + 1]
+
+        if remove_stopwords:
+            words = outtxt.split(" ")
+            newtxt = ""
+            for word in words:
+                if "\n" in word:
+                    split = word.split("\n")
+                    if split[0] not in stop_words:
+                        newtxt += split[0] + "\n"
+                        continue
+                    else:
+                        newtxt += "\n"
+                        continue
+                    if split[1] not in stop_words:
+                        newtxt += split[1] + " "
+                        continue
+                if word not in stop_words:
+                    newtxt += word + " "
+            outtxt = newtxt
+        out = open(dest + doc, "w")
+        out.write(outtxt)
+        out.close()
+        if verbose > 1:
+            print("Processed %s" % doc)
+    if verbose:
+        print("%s documents processed" % len(os.listdir(dest)))
+
+
+def main(process_citations, process_publications, pdf2txt, split_paragraphs, verbose=params["DEFAULT_VERBOSE"]):
+    library_type = params["LIBRARY_TYPE"]
+    api_key = params["ZOTERO_API_KEY"]
+    if library_type == 'group':
+        library_id = params["GROUP_LIBRARY_KEY"]
+        collection_id = params["GROUP_COLLECTION_ID"]
+    elif library_type == 'user':
+        library_id = params["USER_LIBRARY_KEY"]
+        collection_id = params["USER_COLLECTION_ID"]
+    else:
+        raise ValueError("Not an acceptable value for 'LIBRARY_TYPE', should be either 'group' or 'user'")
+
+    items, dataset_list = load_data(params["LOAD_ZOT_JSON"], zotero.Zotero(library_id, library_type, api_key), collection_id)
+
+    articles, notes, attachments, lookup_child_notes, lookup_child_links = sort_zotero_parts(items)
+
+    if process_citations:
+        if verbose:
+            print("START PROCESSING CITATIONS")
+        outfile = os.path.join('data', 'json', 'data_set_citations.json')
+        process_zotero_citations(notes, lookup_child_notes, outfile)
+
+    if process_publications:
+        if verbose:
+            print("START PROCESSING PUBLICATIONS")
+        process_zotero_publications(params["ZOTERO_PATH"], list(articles.keys()), articles, attachments, lookup_child_links)
+
+    if pdf2txt:
+        if verbose:
+            print("CONVERTING PDF TO TXT")
+        convert_pdf2text("data/PDF/", "data/text/") # Takes forever to do, make sure to make this optional in mainfile
+        assert len(os.listdir("data/PDF")) == len(os.listdir("data/text")) # Make sure no doc was lost
+
+    if split_paragraphs:
+        if verbose:
+            print("CONVERTING PDF TO TXT")
+        split_doc_paragraphs("data/PDF", "data/processed_text")
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--load_json'
+    )
+    parser.add_argument(
+        '--process_citations'
+    )
+    parser.add_argument(
+        '--process_publications'
+    )
+    parser.add_argument(
+        '--pdf2txt'
+    )
+    parser.add_argument(
+        '--split_paragraphs'
+    )
+    args = parser.parse_args()
+
+    main(int(args.process_citations), int(args.process_publications), int(args.pdf2txt), int(args.split_paragraphs))
--- preprocessing/utils.py
+++ preprocessing/utils.py
@@ -4,8 +4,16 @@ from pdfminer.converter import XMLConverter, HTMLConverter, TextConverter
 from pdfminer.layout import LAParams
 from io import StringIO
 
+from config import params
 
-def convert_pdf2txt(input_path : str, output_path : str) -> None:
+import xml.etree.ElementTree as ET
+import json
+import requests
+import os
+
+
+def convert_pdf2txt(input_path : str, output_path : str, verbose=params["DEFAULT_VERBOSE"]) -> None:
+  if verbose:
     for file in tqdm(Path(input_path).glob('*.pdf'), ascii=True,desc='pdf->txt'):
       fp = open(file, 'rb')
       rsrcmgr = PDFResourceManager()
@@ -23,7 +31,24 @@ def convert_pdf2txt(input_path : str, output_path : str) -> None:
     if txt_file not in os.listdir(output_path):
       txt_out = open(txt_file, "w")
       txt_out.write(data)
-
+  else:
+    for file in Path(input_path).glob('*.pdf'):
+      fp = open(file, 'rb')
+      rsrcmgr = PDFResourceManager()
+      retstr = StringIO()
+      codec = 'utf-8'
+      laparams = LAParams()
+      device = TextConverter(rsrcmgr, retstr, codec=codec, laparams=laparams)
+      # Create a PDF interpreter object.
+      interpreter = PDFPageInterpreter(rsrcmgr, device)
+      # Process each page contained in the document.
+      for page in PDFPage.get_pages(fp):
+        interpreter.process_page(page)
+        data = retstr.getvalue()
+    txt_file = output_path+file.name+'.txt'
+    if txt_file not in os.listdir(output_path):
+      txt_out = open(txt_file, "w")
+      txt_out.write(data)
 
 def mentions2datasets(path):
   with open(path + "data_sets.json") as json1:
@@ -56,3 +81,61 @@ def search_pubs(pub_list, publication_id):
     if publication['publication_id'] == publication_id:
       return publication
   return False
+
+def create_data_sets(json_cmr_dataset_list, verbose=params["DEFAULT_VERBOSE"]):
+  ns = {'namespace': 'http://gcmd.gsfc.nasa.gov/Aboutus/xml/dif/'}
+
+  dataset_list = []
+  for json_cmr_dataset in json_cmr_dataset_list:
+    if "_NRT" in json_cmr_dataset["short_name"]:
+      continue
+    entry_id = json_cmr_dataset["short_name"] + '_' + json_cmr_dataset["version_id"]
+    p = (
+      ('provider_id', 'GES_DISC'),
+      ('entry_id', entry_id),
+      ('pretty', 'true'),
+    )
+    response = requests.get('https://cmr.earthdata.nasa.gov/search/collections.dif10', params=p)
+    # response = requests.get('https://cmr.earthdata.nasa.gov/search/collections.dif10?provider_id=GES_DISC&entry_id=ML2CO_004&pretty=true')
+    root = ET.fromstring(response.text)
+    DOI = None
+    for child in root:
+      if child.tag == "result":
+        items = child.find("{" + ns.get('namespace') + "}DIF")
+        for item in items:
+          true_tag = item.tag.replace(("{" + ns.get('namespace') + "}"), '')
+          if (true_tag == "Dataset_Citation"):
+            pids = item.find("{" + ns.get('namespace') + "}Persistent_Identifier")
+            for pid in pids:
+              true_tag = pid.tag.replace(("{" + ns.get('namespace') + "}"), '')
+              if true_tag == "Identifier":
+                DOI = pid.text
+                if verbose:
+                  print(DOI)
+
+    dataset_list.append({
+      "data_set_id": json_cmr_dataset["short_name"],
+      "version_id": json_cmr_dataset["version_id"],
+      "unique_identifier": DOI,
+      "title": json_cmr_dataset["dataset_id"],
+      "name": json_cmr_dataset["dataset_id"],
+      "description": json_cmr_dataset["summary"],
+      "date": "",
+      "coverages": "",
+      "subjects": "",
+      "methodology": "",
+      "citation": "",
+      "additional_keywords": "",
+      "family_identifier": "",
+      "mention_list": [],
+      "identifier_list": [
+        {
+          "name": "https://doi.org/",
+          "identifier": DOI
+        }]
+    })
+
+  with open(os.path.join("data","json", "data_sets.json"), "w") as fp:
+    json.dump(dataset_list, fp, indent=4)
+
+  return dataset_list
